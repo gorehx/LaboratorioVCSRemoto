bbed42bcf6253ad8d32c74040d5090ee5f6f2f58
gorehx <47457787+gorehx@users.noreply.github.com>
2019-02-22 19:16:03 -0500

    Mi primer commit

---
+++ main.py
@@ -0,0 +1,6 @@
+a=int(input('Ingrese el primer número'))
+c=int(input('Ingrese el segundo número'))
+prod1= a*c
+print('El producto es', prod1,)
+prod2= a*2
+print('El doble del primer número es:', prod2)
