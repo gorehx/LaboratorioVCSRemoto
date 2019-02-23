e66e964fc585704ca1c1c7e3d0ba2171ffb6d919
gorehx <47457787+gorehx@users.noreply.github.com>
2019-02-22 20:44:44 -0500

    Solución

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-b=int(input('Ingrese el número'))
-prod1= b**2
-print('El cuadrado del número es:', prod1,)
-d=float(input('Ingrese el segundo número para saber su raiz cuadrada'))
-prod2= d**(1/2)
-print('La raíz cuadrada del número es', prod2,)
+a=float(input("Ingrese el número a:"))
+b=float(input("Ingrese el número b:"))
+c=float(input("Ingrese el número c:"))
+d=(((b*2)-(4*a*c))*(1/2))
+neg=(((-1*b)-(d**(1/2))/2*a))
+pos=(((-1*b)+(d**(1/2))/2*a))
+if d>0:
+ print( "La parte negativa es: ", neg , "y la parte postiva es: ", pos)
+else:
+	if d==0:
+		print("El valor negativo: ", (-d/2*a), "es igual al valor positivo, el cual es:", (-d/2*a))
+	else:
+		if d<0:
+			print("No existe solucion en los numeros reales")
