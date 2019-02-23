968d4d00356eda5bf268f01b94e2cf6092f9e990
gorehx <47457787+gorehx@users.noreply.github.com>
2019-02-22 20:16:51 -0500

    Mi primera modificación

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-a=int(input('Ingrese el primer número'))
-c=int(input('Ingrese el segundo número'))
-prod1= a*c
-print('El producto es', prod1,)
-prod2= a*2
-print('El doble del primer número es:', prod2)
+b=int(input('Ingrese el número'))
+prod1= b**2
+print('El cuadrado del número es:', prod1,)
+d=float(input('Ingrese el segundo número para saber su raiz cuadrada'))
+prod2= d**(1/2)
+print('La raíz cuadrada del número es', prod2,)
